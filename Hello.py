257bc014c7e6a2f373e8f3a8b82b9fdf7b7df898
pumplay01 <136250847+pumplay01@users.noreply.github.com>
2023-09-21 15:07:36 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,51 +1,59 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+from transformers import AutoTokenizer, AutoModelForCausalLM
 import streamlit as st
-from streamlit.logger import get_logger
+import random
+import shutil
+import torch
 
-LOGGER = get_logger(__name__)
+st.set_page_config(page_title="HOSPITAL STANDARD AI", page_icon="🏥")
+st.title("HOSPITAL STANDARD AI RECOMMEND")
+st.write("Frist time we need preparation system a few minutes")
 
+tokenizer = AutoTokenizer.from_pretrained("pumplay01/JCI7-GPT-DOCX", bos_token='<|endoftext|>',
+                                          eos_token='<|endoftext|>', pad_token='<|pad|>')
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+# Download the model weights (this is where the actual download happens)
+with st.spinner('Downloading model installation...'):
+    model = AutoModelForCausalLM.from_pretrained("pumplay01/JCI7-GPT-DOCX")
 
-    st.write("# Welcome to Streamlit! 👋")
+# Once the download is complete, display a success message
+st.success("Model downloaded successfully!")
 
-    st.sidebar.success("Select a demo above.")
+# Add input fields for user parameters
+inputx = st.text_input("Input Text:", "Type a word at here")
+max_length_num = st.number_input("Max Length:", min_value=1, value=310)
+num_samples = st.number_input("Number of Answer:", min_value=1, value=10)
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+if st.button("Generate Text"):
+    # Call your code with user-provided parameters
+    generated_results = []
 
+    generated = tokenizer(f"{inputx}", return_tensors="pt").input_ids
+    sample_outputs = model.generate(generated,
+                                    do_sample=True,
+                                    top_k=500,
+                                    max_length=max_length_num,
+                                    top_p=0.95,
+                                    temperature=1.5,
+                                    num_return_sequences=num_samples)
 
-if __name__ == "__main__":
-    run()
+    for sample_output in sample_outputs:
+        generated_text = tokenizer.decode(sample_output, skip_special_tokens=True)
+        generated_results.append(generated_text)
+
+    # Shuffle the generated results
+    random.shuffle(generated_results)
+
+    # Combine all generated results into one text with spaces between lines
+    combined_results = ' '.join(generated_results)
+
+    # Get the width of the terminal window
+    terminal_width = shutil.get_terminal_size().columns
+
+    # Define the line length to be 80% of the terminal width
+    line_length = int(terminal_width * 1.2)
+
+    # Split the text into lines
+    lines = [combined_results[i:i+line_length] for i in range(0, len(combined_results), line_length)]
+
+    # Print the combined results
+    st.write(combined_results, unsafe_allow_html=True)
